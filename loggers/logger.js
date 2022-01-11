8f9effb65bb042f829b0a4cd7e9f5901263f7bb4
Meet Thakkar <meetthakkar@Meet-MacBook-pro.local>
2022-01-11 10:53:01 +0530

    fix(logger): fix some mistakes in logger file

--- loggers/logger.js
+++ loggers/logger.js
@@ -6,7 +6,7 @@ const { timestamp, combine, json, errors } = format;
 const logger = createLogger({
   format: combine(timestamp(), errors({ stack: true }), json()),
   // defaultMeta: { service: 'user-service' },
-  transports: [new transports.Console(), new transports.File({ filename: 'logger/prod-loggings.log' })],
+  transports: [new transports.Console(), new transports.File({ filename: 'loggers/loggings.log' })],
 });
 
 module.exports = logger;
